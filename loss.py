0568596dc97cde1e893d31e973592cdc3d7ce63c
Mikhaylov Artyom <SimpleRick@yandex.ru>
2020-02-22 16:58:54 +0300

    Fixed scale

--- loss.py
+++ loss.py
@@ -15,12 +15,18 @@ def D_logistic(real_logits, fake_logits):
 
 
 def R1_reg(real_imgs, real_logits):
+    '''
+    R1 regularization
+    '''
     grads = torch.autograd.grad(real_logits.sum(), real_imgs, create_graph=True)[0]
-    return torch.mean(grads**2)
+    return torch.mean((grads**2).sum(dim=[1,2,3]))
 
 
 
 class Path_length_loss(nn.Module):
+    '''
+    Path length regularization
+    '''
     def __init__(self, decay=0.01):
         super().__init__()
         self.decay = decay
@@ -30,7 +36,7 @@ class Path_length_loss(nn.Module):
         # Compute |J*y|.
         noise = torch.randn(gen_out.shape)/np.sqrt(np.prod(gen_out.shape[2:])) #[N,Channels,H,W]
         grads = torch.autograd.grad((gen_out * noise).sum(), dlatent, create_graph=True)[0]  #[N,dlatent_size]
-        lengths = torch.sqrt((grads**2).sum(1)) #[N]
+        lengths = torch.sqrt((grads**2).mean(1)) #[N]
         # Update exp average. Lengths are detached
         self.avg = self.decay*torch.mean(lengths.detach()) + (1-self.decay)*self.avg
         return torch.mean((lengths - self.avg)**2)
